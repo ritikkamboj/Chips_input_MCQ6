33ee7ea4087fed6059a0854a6c3e8b349947080c
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-07-16 19:10:56 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [input , setInput] = useState("")
+
+  return (
+    <div>
+      <h1 className='heading'>Chips Input</h1>
+    </div>
+   
+  )
+}
+
+export default App
