362296e641616a4601ac6b3306ff05b40bc7bc74
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-07-16 19:39:22 +0530

    added chips successfully

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,28 @@ import './App.css'
 
 function App() {
   const [input , setInput] = useState("")
+  const [data , setData] = useState(['akshay' , 'saini'])
+
+  function handleAddition(e)
+  {
+    if(e.key === "Enter" && input.trim() !== "")
+    {
+      setData((prev)=> [...prev , input]);
+            setInput("")
+
+    }
+
+
+  }
 
   return (
-    <div>
+    <div className='app'>
       <h1 className='heading'>Chips Input</h1>
+      <input type="text" placeholder='Enter Your Text' className='input' onChange={(e) => setInput(e.target.value) } onKeyDown={(e)=>handleAddition(e)}  />
+      <div className='dis-chip'>
+        {data.map((item)=> <div className='chip' key={item} >{item} <button onClick={}>❌</button> </div>)}
+
+      </div>
     </div>
    
   )
